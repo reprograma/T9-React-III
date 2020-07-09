e4a7830a4d75385d027f695dc16783b120842832
oskojess <jessica_osko@hotmail.com>
2020-07-09 16:05:42 -0300

    adicionar botoes

--- aula-02/src/pages/Main/index.js
+++ aula-02/src/pages/Main/index.js
@@ -7,25 +7,50 @@ import './styles.css'
 
 class Main extends Component {
   state = {
-    biographies: []
+    biographies: [],
+    biographyInfo: {},
+    page: 1
   }
 
   componentDidMount() {
     this.loadBiographies();
   }
 
-  loadBiographies = async () => {
+  loadBiographies = async (page = 1) => {
     //resposta da minha API = response/resposta
-    const response = await api.get(`/biographies`)
+    const response = await api.get(`/biographies?page=${page}`)
 
-    const { docs } = response.data
+    const { docs, ...biographyInfo } = response.data
 
-    this.setState({ biographies: docs })
+    this.setState({ biographies: docs, page, biographyInfo })
   }
 
-  render() {
-    const { biographies } = this.state;
 
+  prevPage = () => {
+    const { page } = this.state;
+
+    if (page === 1) return;
+
+    const pageNumber = page - 1;
+
+    this.loadBiographies(pageNumber)
+
+  }
+
+  nextPage = () => {
+    const { page, biographyInfo } = this.state;
+
+    if (page === biographyInfo.pages) return;
+
+    const pageNumber = page + 1;
+
+    this.loadBiographies(pageNumber)
+  }
+
+
+
+  render() {
+    const { biographies, page, biographyInfo } = this.state;
     return (
       <div className="biography-list">
         {biographies.map(biography => (
@@ -35,6 +60,11 @@ class Main extends Component {
             <Link to={`/biography/${biography._id}`}>Acessar</Link>
           </article>
         ))}
+
+        <div className="actions">
+          <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+          <button disabled={page === biographyInfo.pages} onClick={this.nextPage}>Próximo</button>
+        </div>
       </div>
     )
   }
